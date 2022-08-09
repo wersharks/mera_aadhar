ffb0c9596532c3ec22ae319ea421fba7583d8262
RGTechPro <73791635+RGTechPro@users.noreply.github.com>
2022-08-10 03:13:30 +0530

    structure added

---
+++ lib/screens/verification.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class Verification extends StatefulWidget {
+  const Verification({Key? key}) : super(key: key);
+
+  @override
+  State<Verification> createState() => _VerificationState();
+}
+
+class _VerificationState extends State<Verification> {
+  @override
+  Widget build(BuildContext context) {
+    return
+    Column(
+      
+    )
+    ;
+  }
+}
